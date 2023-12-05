ad534016a302899c3539ad395aa31129d0b4bf8f
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-12-05 10:18:32 +0100

    Fix bug

--- Src/ciine-gp-joys/V2/ciine-gp-joys/Joyconright/Joyconright.cs
+++ Src/ciine-gp-joys/V2/ciine-gp-joys/Joyconright/Joyconright.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,114 @@ namespace JoyconRightAPI
 {
     public class JoyconRight
     {
+        [DllImport("MotionInputPairing.dll", EntryPoint = "joyconrightconnect")]
+        public static extern bool joyconrightconnect();
+        [DllImport("MotionInputPairing.dll", EntryPoint = "joyconrightdisconnect")]
+        public static extern bool joyconrightdisconnect();
+        [DllImport("rhidread.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "Rhid_read_timeout")]
+        public static extern int Rhid_read_timeout(SafeFileHandle dev, byte[] data, UIntPtr length);
+        [DllImport("rhidread.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "Rhid_write")]
+        public static extern int Rhid_write(SafeFileHandle device, byte[] data, UIntPtr length);
+        [DllImport("rhidread.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "Rhid_open_path")]
+        public static extern SafeFileHandle Rhid_open_path(IntPtr handle);
+        [DllImport("rhidread.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "Rhid_close")]
+        public static extern void Rhid_close(SafeFileHandle device);
+        [DllImport("hid.dll")]
+        public static extern void HidD_GetHidGuid(out Guid gHid);
+        [DllImport("hid.dll")]
+        public extern static bool HidD_SetOutputReport(IntPtr HidDeviceObject, byte[] lpReportBuffer, uint ReportBufferLength);
+        [DllImport("setupapi.dll")]
+        public static extern IntPtr SetupDiGetClassDevs(ref Guid ClassGuid, string Enumerator, IntPtr hwndParent, UInt32 Flags);
+        [DllImport("setupapi.dll")]
+        public static extern Boolean SetupDiEnumDeviceInterfaces(IntPtr hDevInfo, IntPtr devInvo, ref Guid interfaceClassGuid, Int32 memberIndex, ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData);
+        [DllImport("setupapi.dll")]
+        public static extern Boolean SetupDiGetDeviceInterfaceDetail(IntPtr hDevInfo, ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData, IntPtr deviceInterfaceDetailData, UInt32 deviceInterfaceDetailDataSize, out UInt32 requiredSize, IntPtr deviceInfoData);
+        [DllImport("setupapi.dll")]
+        public static extern Boolean SetupDiGetDeviceInterfaceDetail(IntPtr hDevInfo, ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData, ref SP_DEVICE_INTERFACE_DETAIL_DATA deviceInterfaceDetailData, UInt32 deviceInterfaceDetailDataSize, out UInt32 requiredSize, IntPtr deviceInfoData);
+        [DllImport("Kernel32.dll")]
+        public static extern SafeFileHandle CreateFile(string fileName, [MarshalAs(UnmanagedType.U4)] FileAccess fileAccess, [MarshalAs(UnmanagedType.U4)] FileShare fileShare, IntPtr securityAttributes, [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition, [MarshalAs(UnmanagedType.U4)] uint flags, IntPtr template);
+        [DllImport("Kernel32.dll")]
+        public static extern IntPtr CreateFile(string fileName, System.IO.FileAccess fileAccess, System.IO.FileShare fileShare, IntPtr securityAttributes, System.IO.FileMode creationDisposition, EFileAttributes flags, IntPtr template);
+        public const uint report_lenRight = 49;
+        public byte[] report_bufRight = new byte[report_lenRight];
+        public SafeFileHandle handleRight;
+        public void Close()
+        {
+            Rhid_close(handleRight);
+            handleRight.Close();
+            handleRight.Dispose();
+            joyconrightdisconnect();
+        }
+        public void BeginAsyncPolling()
+        {
+            Rhid_read_timeout(handleRight, report_bufRight, (UIntPtr)report_lenRight);
+        }
+        public const string vendor_id = "57e", vendor_id_ = "057e", product_l = "2006", product_r = "2007";
+        public enum EFileAttributes : uint
+        {
+            Overlapped = 0x40000000,
+            Normal = 0x80
+        };
+        public struct SP_DEVICE_INTERFACE_DATA
+        {
+            public int cbSize;
+            public Guid InterfaceClassGuid;
+            public int Flags;
+            public IntPtr RESERVED;
+        }
+        public struct SP_DEVICE_INTERFACE_DETAIL_DATA
+        {
+            public UInt32 cbSize;
+            [System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string DevicePath;
+        }
+        public bool ScanRight()
+        {
+            joyconrightconnect();
+            int index = 0;
+            System.Guid guid;
+            HidD_GetHidGuid(out guid);
+            System.IntPtr hDevInfo = SetupDiGetClassDevs(ref guid, null, new System.IntPtr(), 0x00000010);
+            SP_DEVICE_INTERFACE_DATA diData = new SP_DEVICE_INTERFACE_DATA();
+            diData.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(diData);
+            while (SetupDiEnumDeviceInterfaces(hDevInfo, new System.IntPtr(), ref guid, index, ref diData))
+            {
+                System.UInt32 size;
+                SetupDiGetDeviceInterfaceDetail(hDevInfo, ref diData, new System.IntPtr(), 0, out size, new System.IntPtr());
+                SP_DEVICE_INTERFACE_DETAIL_DATA diDetail = new SP_DEVICE_INTERFACE_DETAIL_DATA();
+                diDetail.cbSize = 5;
+                if (SetupDiGetDeviceInterfaceDetail(hDevInfo, ref diData, ref diDetail, size, out size, new System.IntPtr()))
+                {
+                    if ((diDetail.DevicePath.Contains(vendor_id) | diDetail.DevicePath.Contains(vendor_id_)) & diDetail.DevicePath.Contains(product_r))
+                    {
+                        AttachJoyRight(diDetail.DevicePath);
+                        return true;
+                    }
+                }
+                index++;
+            }
+            return false;
+        }
+        public void AttachJoyRight(string path)
+        {
+            do
+            {
+                IntPtr handle = CreateFile(path, System.IO.FileAccess.ReadWrite, System.IO.FileShare.ReadWrite, new System.IntPtr(), System.IO.FileMode.Open, EFileAttributes.Normal, new System.IntPtr());
+                handleRight = Rhid_open_path(handle);
+                SubcommandRight(0x40, new byte[] { 0x1 }, 1);
+                SubcommandRight(0x3, new byte[] { 0x30 }, 1);
+            }
+            while (handleRight.IsInvalid);
+        }
+        public void SubcommandRight(byte sc, byte[] buf, uint len)
+        {
+            byte[] buf_Right = new byte[report_lenRight];
+            System.Array.Copy(buf, 0, buf_Right, 11, len);
+            buf_Right[10] = sc;
+            buf_Right[1] = 0;
+            buf_Right[0] = 0x1;
+            Rhid_write(handleRight, buf_Right, (UIntPtr)(len + 11));
+            Rhid_read_timeout(handleRight, buf_Right, (UIntPtr)report_lenRight);
+        }
     }
 }
--- Src/ciine-gp-spc/V2/ciine-gp-spc/Switchprocontroller/Switchprocontroller.cs
+++ Src/ciine-gp-spc/V2/ciine-gp-spc/Switchprocontroller/Switchprocontroller.cs
@@ -1,12 +1,152 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace SwitchProControllerAPI
 {
     public class SwitchProController
     {
+        [DllImport("hid.dll")]
+        public static extern void HidD_GetHidGuid(out Guid gHid);
+        [DllImport("hid.dll")]
+        public static extern bool HidD_SetOutputReport(IntPtr HidDeviceObject, byte[] lpReportBuffer, uint ReportBufferLength);
+        [DllImport("setupapi.dll")]
+        public static extern IntPtr SetupDiGetClassDevs(ref Guid ClassGuid, string Enumerator, IntPtr hwndParent, UInt32 Flags);
+        [DllImport("setupapi.dll")]
+        public static extern Boolean SetupDiEnumDeviceInterfaces(IntPtr hDevInfo, IntPtr devInvo, ref Guid interfaceClassGuid, Int32 memberIndex, ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData);
+        [DllImport("setupapi.dll")]
+        public static extern Boolean SetupDiGetDeviceInterfaceDetail(IntPtr hDevInfo, ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData, IntPtr deviceInterfaceDetailData, UInt32 deviceInterfaceDetailDataSize, out UInt32 requiredSize, IntPtr deviceInfoData);
+        [DllImport("setupapi.dll")]
+        public static extern Boolean SetupDiGetDeviceInterfaceDetail(IntPtr hDevInfo, ref SP_DEVICE_INTERFACE_DATA deviceInterfaceData, ref SP_DEVICE_INTERFACE_DETAIL_DATA deviceInterfaceDetailData, UInt32 deviceInterfaceDetailDataSize, out UInt32 requiredSize, IntPtr deviceInfoData);
+        [DllImport("Kernel32.dll")]
+        public static extern SafeFileHandle CreateFile(string fileName, [MarshalAs(UnmanagedType.U4)] FileAccess fileAccess, [MarshalAs(UnmanagedType.U4)] FileShare fileShare, IntPtr securityAttributes, [MarshalAs(UnmanagedType.U4)] FileMode creationDisposition, [MarshalAs(UnmanagedType.U4)] uint flags, IntPtr template);
+        [DllImport("Kernel32.dll")]
+        public static extern IntPtr CreateFile(string fileName, System.IO.FileAccess fileAccess, System.IO.FileShare fileShare, IntPtr securityAttributes, System.IO.FileMode creationDisposition, EFileAttributes flags, IntPtr template);
+        [DllImport("prohidread.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "Prohid_read_timeout")]
+        public static extern int Prohid_read_timeout(SafeFileHandle dev, byte[] data, UIntPtr length);
+        [DllImport("prohidread.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "Prohid_write")]
+        public static extern int Prohid_write(SafeFileHandle device, byte[] data, UIntPtr length);
+        [DllImport("prohidread.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "Prohid_open_path")]
+        public static extern SafeFileHandle Prohid_open_path(IntPtr handle);
+        [DllImport("prohidread.dll", CallingConvention = CallingConvention.Cdecl, EntryPoint = "Prohid_close")]
+        public static extern void Prohid_close(SafeFileHandle device);
+        public const uint report_lenPro = 49;
+        public byte[] report_bufPro = new byte[report_lenPro];
+        public SafeFileHandle handlePro;
+        public void Close()
+        {
+            Subcommand3ProController(0x06, new byte[] { 0x01 }, 1);
+            Prohid_close(handlePro);
+            handlePro.Close();
+            handlePro.Dispose();
+        }
+        public void BeginAsyncPolling()
+        {
+            Prohid_read_timeout(handlePro, report_bufPro, (UIntPtr)report_lenPro);
+        }
+        public const string vendor_id = "57e", vendor_id_ = "057e", product_pro = "2009";
+        public enum EFileAttributes : uint
+        {
+            Overlapped = 0x40000000,
+            Normal = 0x80
+        };
+        public struct SP_DEVICE_INTERFACE_DATA
+        {
+            public int cbSize;
+            public Guid InterfaceClassGuid;
+            public int Flags;
+            public IntPtr RESERVED;
+        }
+        public struct SP_DEVICE_INTERFACE_DETAIL_DATA
+        {
+            public UInt32 cbSize;
+            [System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string DevicePath;
+        }
+        public bool ScanPro()
+        {
+            int index = 0;
+            System.Guid guid;
+            HidD_GetHidGuid(out guid);
+            System.IntPtr hDevInfo = SetupDiGetClassDevs(ref guid, null, new System.IntPtr(), 0x00000010);
+            SP_DEVICE_INTERFACE_DATA diData = new SP_DEVICE_INTERFACE_DATA();
+            diData.cbSize = System.Runtime.InteropServices.Marshal.SizeOf(diData);
+            while (SetupDiEnumDeviceInterfaces(hDevInfo, new System.IntPtr(), ref guid, index, ref diData))
+            {
+                System.UInt32 size;
+                SetupDiGetDeviceInterfaceDetail(hDevInfo, ref diData, new System.IntPtr(), 0, out size, new System.IntPtr());
+                SP_DEVICE_INTERFACE_DETAIL_DATA diDetail = new SP_DEVICE_INTERFACE_DETAIL_DATA();
+                diDetail.cbSize = 5;
+                if (SetupDiGetDeviceInterfaceDetail(hDevInfo, ref diData, ref diDetail, size, out size, new System.IntPtr()))
+                {
+                    if ((diDetail.DevicePath.Contains(vendor_id) | diDetail.DevicePath.Contains(vendor_id_)) & diDetail.DevicePath.Contains(product_pro))
+                    {
+                        AttachProController(diDetail.DevicePath);
+                        return true;
+                    }
+                }
+                index++;
+            }
+            return false;
+        }
+        private void AttachProController(string path)
+        {
+            do
+            {
+                IntPtr handle = CreateFile(path, System.IO.FileAccess.ReadWrite, System.IO.FileShare.ReadWrite, new System.IntPtr(), System.IO.FileMode.Open, EFileAttributes.Normal, new System.IntPtr());
+                handlePro = Prohid_open_path(handle);
+                Subcommand1ProController(0x06, new byte[] { 0x01 }, 1);
+                Subcommand2ProController(0x40, new byte[] { 0x1 }, 1);
+                Subcommand2ProController(0x3, new byte[] { 0x30 }, 1);
+                Subcommand2ProController(0x40, new byte[] { 0x1 }, 1);
+                Subcommand2ProController(0x3, new byte[] { 0x30 }, 1);
+            }
+            while (handlePro.IsInvalid);
+        }
+        private void Subcommand1ProController(byte sc, byte[] buf, uint len)
+        {
+            byte[] buf_Pro = new byte[report_lenPro];
+            System.Array.Copy(buf, 0, buf_Pro, 11, len);
+            buf_Pro[10] = sc;
+            buf_Pro[1] = 0x2;
+            buf_Pro[0] = 0x80;
+            Prohid_write(handlePro, buf_Pro, new UIntPtr(2));
+            Prohid_read_timeout(handlePro, buf_Pro, (UIntPtr)report_lenPro);
+            buf_Pro[1] = 0x3;
+            buf_Pro[0] = 0x80;
+            Prohid_write(handlePro, buf_Pro, new UIntPtr(2));
+            Prohid_read_timeout(handlePro, buf_Pro, (UIntPtr)report_lenPro);
+            buf_Pro[1] = 0x2;
+            buf_Pro[0] = 0x80;
+            Prohid_write(handlePro, buf_Pro, new UIntPtr(2));
+            Prohid_read_timeout(handlePro, buf_Pro, (UIntPtr)report_lenPro);
+            buf_Pro[1] = 0x4;
+            buf_Pro[0] = 0x80;
+            Prohid_write(handlePro, buf_Pro, new UIntPtr(2));
+            Prohid_read_timeout(handlePro, buf_Pro, (UIntPtr)report_lenPro);
+        }
+        private void Subcommand2ProController(byte sc, byte[] buf, uint len)
+        {
+            byte[] buf_Pro = new byte[report_lenPro];
+            System.Array.Copy(buf, 0, buf_Pro, 11, len);
+            buf_Pro[10] = sc;
+            buf_Pro[1] = 0;
+            buf_Pro[0] = 0x1;
+            Prohid_write(handlePro, buf_Pro, (UIntPtr)(len + 11));
+            Prohid_read_timeout(handlePro, buf_Pro, (UIntPtr)report_lenPro);
+        }
+        private void Subcommand3ProController(byte sc, byte[] buf, uint len)
+        {
+            byte[] buf_Pro = new byte[report_lenPro];
+            System.Array.Copy(buf, 0, buf_Pro, 11, len);
+            buf_Pro[10] = sc;
+            buf_Pro[1] = 0x5;
+            buf_Pro[0] = 0x80;
+            Prohid_write(handlePro, buf_Pro, new UIntPtr(2));
+            buf_Pro[1] = 0x6;
+            buf_Pro[0] = 0x80;
+            Prohid_write(handlePro, buf_Pro, new UIntPtr(2));
+        }
     }
 }
