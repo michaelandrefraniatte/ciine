bcf64129ad8d235e6de3c1ab6a27fd32a1e3cede
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-12-31 16:27:53 +0100

    Remove useless

--- Src/ciine-gp-wn-mw2/V15/ciine/ciine/Program.cs
+++ Src/ciine-gp-wn-mw2/V15/ciine/ciine/Program.cs
@@ -1,17 +1,75 @@
+using System.Diagnostics;
+using System.Security.Principal;
+
 namespace ciine
 {
     internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Point d'entrée principal de l'application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
+            bool runelevated = true;
+            bool oneinstanceonly = true;
+            try
+            {
+                if (oneinstanceonly)
+                {
+                    if (AlreadyRunning())
+                    {
+                        return;
+                    }
+                }
+                if (runelevated)
+                {
+                    if (!hasAdminRights())
+                    {
+                        RunElevated();
+                        return;
+                    }
+                }
+            }
+            catch
+            {
+                return;
+            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+        public static bool hasAdminRights()
+        {
+            WindowsPrincipal principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+        public static void RunElevated()
+        {
+            try
+            {
+                ProcessStartInfo processInfo = new ProcessStartInfo();
+                processInfo.Verb = "runas";
+                processInfo.FileName = Application.ExecutablePath;
+                Process.Start(processInfo);
+            }
+            catch { }
+        }
+        private static void SetProcessPriority()
+        {
+            using (Process p = Process.GetCurrentProcess())
+            {
+                p.PriorityClass = ProcessPriorityClass.RealTime;
+            }
+        }
+        private static bool AlreadyRunning()
+        {
+            String thisprocessname = Process.GetCurrentProcess().ProcessName;
+            Process[] processes = Process.GetProcessesByName(thisprocessname);
+            if (processes.Length > 1)
+                return true;
+            else
+                return false;
         }
     }
 }
--- Src/ciine-gp-wn-mw2/V15/ciine/ciine/obj/Release/net8.0-windows/ciine.AssemblyInfo.cs
+++ Src/ciine-gp-wn-mw2/V15/ciine/ciine/obj/Release/net8.0-windows/ciine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ciine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2718ab0959b5c5a621b4e53416a947c65fa5e4ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ciine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ciine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
