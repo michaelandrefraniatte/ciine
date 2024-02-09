2f0ce9b5102900b0c0d13774cb0b9fe91479a757
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-02-09 18:46:25 +0100

    Add about

--- Src/ciine-gp-wn-mw2/ciine/ciine/Form1.cs
+++ Src/ciine-gp-wn-mw2/ciine/ciine/Form1.cs
@@ -30,6 +30,23 @@ namespace ciine
         public static Valuechange ValueChange = new Valuechange();
         private WiiMote wm = new WiiMote();
         private XBoxController scp = new XBoxController();
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            OnKeyDown(e.KeyData);
+        }
+        private void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if (keyData == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             TimeBeginPeriod(1);
