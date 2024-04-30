f48cdb241489695e16ed097c9b7f391cdbc1e245
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2024-04-30 18:50:36 +0200

    Add about

--- Src/ciine-lg-jcg/ciine-lg-jcg/Form1.cs
+++ Src/ciine-lg-jcg/ciine-lg-jcg/Form1.cs
@@ -7,6 +7,15 @@ namespace ciine_lg_jcg
 {
     public partial class Form1 : Form
     {
+        static void OnKeyDown(Keys keyData)
+        {
+            if (keyData == Keys.F1)
+            {
+                const string message = "• Author: Michaël André Franiatte.\n\r\n\r• Contact: michael.franiatte@gmail.com.\n\r\n\r• Publisher: https://github.com/michaelandrefraniatte.\n\r\n\r• Copyrights: All rights reserved, no permissions granted.\n\r\n\r• License: Not open source, not free of charge to use.";
+                const string caption = "About";
+                MessageBox.Show(message, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
         public Form1()
         {
             InitializeComponent();
